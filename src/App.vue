278613a381c3e56e0cb51cbf6f267032e89a0952
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2023-09-07 09:53:09 +0700

    separate component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="wrapper">
+    <the-header></the-header>
+    <div id="dashboard">
+      <the-side-bar></the-side-bar>
+      <router-view />
+    </div>
+  </div>
 </template>
+<script>
+import TheHeader from './components/TheHeader.vue'
+import TheSideBar from './components/TheSideBar.vue'
+export default {
+  components: {
+    TheHeader,
+    TheSideBar
+  },
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
+</script>
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
